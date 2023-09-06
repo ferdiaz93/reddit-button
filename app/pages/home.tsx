a0d0e4cffc595ad80e4651892d68e6de9495e1da
Fernando Diaz <fernandodiaz1209@gmail.com>
2023-09-06 16:18:57 -0300

    refactor

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -8,44 +8,61 @@ import Counter from "../components/Counter";
 import Notification from "../components/Notification";
 
 const HomePage = () => {
-  const [assignedColor, setAssignedColor] = useLocalStorage('assignedColor', '')
-  const [clicked, setClicked] = useState(false);
-  const [color, setColor] = useState("");
+  const [assignedColor, setAssignedColor] = useLocalStorage('assigned-color', '');
+  const [delayedTime, setDelayedTime] = useLocalStorage('delayed-time', '');
   const [timeLeft, setTimeLeft] = useState(60);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (!clicked && !assignedColor) {
-      const timer = setInterval(() => {
-        setTimeLeft((prevTime) => prevTime === 0 ? 60 : prevTime - 1);
-      }, 1000);
-
-      return () => clearInterval(timer);
+    // Si ya tiene un color asignado, corta la ejecucion
+    if (!!delayedTime && !!assignedColor) {
+      setTimeLeft(parseInt(delayedTime));
+      return
     }
-  }, [timeLeft, clicked, assignedColor]);
+
+    // Temporizador
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime === 0 ? 60 : prevTime - 1);
+    }, 1000);
+
+
+
+
+    setLoading(false);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timeLeft, assignedColor, delayedTime]);
 
   const handleClick = () => {
-    if (!clicked) {
-      setClicked(true);
+    if (!assignedColor) {
       const colorSelected = getColorBySecond(timeLeft);
-      setColor(colorSelected);
-      setAssignedColor(colorSelected)
+      setAssignedColor(colorSelected);
+      setDelayedTime(`${timeLeft}`)
     }
   };
 
   return (
     <section className="flex flex-col gap-y-10 items-center">
-      <Counter timeLeft={timeLeft} />
-      <Button onClick={handleClick} disabled={clicked || !!assignedColor}>
-        <span className="absolute">
-          {clicked || assignedColor ? "Pressed" : "Press"}
-        </span>
-      </Button>
-      {color && (
-        <article
-          style={{ backgroundColor: color, width: "100px", height: "100px" }}
-        ></article>
-        )}
-      <Notification text={'Fernando clickeo el boton!'}></Notification>
+      {loading ? (
+        <div className="text-5xl font-bold">Loading...</div>
+      ) : (
+        <div className="flex flex-col gap-y-10 items-center">
+          <Counter timeLeft={timeLeft} />
+          <Button onClick={handleClick} disabled={!!assignedColor}>
+            <span className="absolute">
+              {!!assignedColor ? "Pressed" : "Press"}
+            </span>
+          </Button>
+          <Notification text={'Fernando clickeo el boton!'}></Notification>
+          {!!assignedColor && (
+            <article
+              className="text-5xl font-bold flex items-center justify-center"
+              style={{ color: assignedColor, height: "100px" }}
+            >Este es tu nuevo color de usuario</article>
+            )}
+        </div>
+      )}
     </section>
   );
 };
